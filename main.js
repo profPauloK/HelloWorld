5fda5bbf75fd32ef4c0afb8215b7760dc6583070
profPauloK <kretikouski@prof.educacao.sp.gov.br>
2024-09-17 13:53:13 -0300

    05-07

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Olá mundo!");
+//alert("Olá mundo!");
 let nomeUsuário = prompt("Qual o seu nome?");
 let elemento = document.querySelector("#nome-usuário");
 elemento.textContent = nomeUsuário;
